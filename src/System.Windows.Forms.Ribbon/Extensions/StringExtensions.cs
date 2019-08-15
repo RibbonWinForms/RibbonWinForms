923397ea3a5d6732a3c2074d2d637958f8f4d3a5
tajbender <tajbender@electrifier.org>
2019-08-15 08:01:29 +0200

    Add ExtensionAttribute for .NET2.0 compatibility, add StringExtensions.cs - [Note: This will allow usage of Extensions with .NET 2.0]

---
+++ src/System.Windows.Forms.Ribbon/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿// *********************************
+// Message from Original Author:
+//
+// 2008 Jose Menendez Poo
+// Please give me credit if you use this code. It's all I ask.
+// Contact me for more info: menendezpoo@gmail.com
+// *********************************
+//
+// Original project from http://ribbon.codeplex.com/
+// Continue to support and maintain by http://officeribbon.codeplex.com/
+
+namespace System.Windows.Forms.Extensions
+{
+#if (NET20 || NET35)
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Indicates whether a specified string is null, empty, or consists only of white-space characters.
+        /// </summary>
+        /// <param name="value">The string to test.</param>
+        /// <returns>true if the value parameter is null or Empty, or if value consists exclusively of white-space characters.</returns>
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrEmpty(value) || value.Trim().Length == 0;
+        }
+    }
+#endif
+}
---
+++ src/System.Windows.Forms.Ribbon/System.Runtime.CompilerServices.cs
@@ -0,0 +1,28 @@
+﻿// *********************************
+// Message from Original Author:
+//
+// 2008 Jose Menendez Poo
+// Please give me credit if you use this code. It's all I ask.
+// Contact me for more info: menendezpoo@gmail.com
+// *********************************
+//
+// Original project from http://ribbon.codeplex.com/
+// Continue to support and maintain by http://officeribbon.codeplex.com/
+
+
+namespace System.Runtime.CompilerServices
+{
+#if NET20
+    /// <summary>
+    /// Extension methods require an attribute which is normally part of .NET 3.5.
+    /// 
+    /// By adding this attribute, we'll get Extension methods to work with .NET 2.0.
+    /// 
+    /// tajbender 15/08/19: I'm really unaware if this will bring us trouble, however, I think it's worth a try
+    /// <see href="https://csharpindepth.com/Articles/Versions"/>
+    /// <see href="https://blogs.msmvps.com/punitganshani/2015/06/21/5-steps-to-targeting-multiple-net-frameworks/"/>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
+    public class ExtensionAttribute : Attribute { }
+#endif
+}
